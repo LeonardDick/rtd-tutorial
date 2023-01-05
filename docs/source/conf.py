0f00914736ac7d323ebc811d6e02f2df7e1553a7
LeonardDick <dick@thch.uni-bonn.de>
2023-01-05 14:07:42 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinxcontrib.bibtex',
+    'sphinxcontrib.bibtex'
 ]
 
 bibtex_bibfiles = ['refs.bib']
